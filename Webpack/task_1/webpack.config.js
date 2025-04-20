0733da2d356977457daea40e196381432455e384
Khaled-J7 <6711@holbertonstudents.com>
2025-04-20 15:22:13 +0100

    Task 1: Learning how to use Webpack with a config file

---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
