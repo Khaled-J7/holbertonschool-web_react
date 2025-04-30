cb2f42ec691c9051914be923560d2278ba03bd16
Khaled-J7 <6711@holbertonstudents.com>
2025-04-30 16:01:07 +0100

    Create a function getLatestNotification

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,3 +1,5 @@
+// utils.js
+
 // Return the current year
 export function getCurrentYear() {
   return new Date().getFullYear();
@@ -6,4 +8,9 @@ export function getCurrentYear() {
 // Return different text based on whether we're on index page or not
 export function getFooterCopy(isIndex) {
   return isIndex ? "Holberton School" : "Holberton School main dashboard";
+}
+
+// New function to get the latest notification
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
 }
