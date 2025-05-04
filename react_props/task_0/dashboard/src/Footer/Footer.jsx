87765eb30f74e420aec5040a6b4ca9e9b1cfd821
Khaled-J7 <6711@holbertonstudents.com>
2025-05-04 23:25:45 +0100

    0. Basic components

---
+++ react_props/task_0/dashboard/src/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+import { getFooterCopy, getFullYear } from "../utils/utils";
+
+function Footer() {
+  let footertext = getFooterCopy(true);
+  return (
+    <div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {footertext}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
