669ee2fedd24c4b17b1e302e0bef704644fa8202
Khaled-J7 <6711@holbertonstudents.com>
2025-05-12 18:36:53 +0100

    Update the tests

--- react_component/task_3/dashboard/src/App/App.spec.js
+++ react_component/task_3/dashboard/src/App/App.spec.js
@@ -23,4 +23,25 @@ describe("App Component", () => {
     await user.keyboard("{Control>}{h}{/Control}");
     expect(mockFunction).toHaveBeenCalled();
   });
+
+  test("displays 'Course list' title above CourseList when isLoggedIn is true", () => {
+    render(<App isLoggedIn={true} />);
+    const heading = screen.getByRole("heading", { name: /Course list/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("displays 'Log in to continue' title above Login when isLoggedIn is false", () => {
+    render(<App isLoggedIn={false} />);
+    const heading = screen.getByRole("heading", { name: /Log in to continue/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("displays 'News from the School' title and news paragraph by default", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { name: /News from the School/i });
+    const paragraph = screen.getByText(/Holberton School News goes here/i);
+
+    expect(heading).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
 });
