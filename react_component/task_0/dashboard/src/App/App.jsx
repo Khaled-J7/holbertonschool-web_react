4b6fca0da589456c4860f0eae28b1d4e12da3218
Khaled-J7 <6711@holbertonstudents.com>
2025-05-12 15:30:53 +0100

    0. Switch to class components

---
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./App.css";
+import Notifications from "../Notifications/Notifications.jsx";
+import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
+import Login from "../Login/Login.jsx";
+import CourseList from "../CourseList/CourseList.jsx";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils.js";
+import { v4 as uuidv4 } from "uuid";
+
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const notificationsList = [
+      { id: uuidv4(), type: "default", value: "New course available" },
+      { id: uuidv4(), type: "urgent", value: "New resume available" },
+      { id: uuidv4(), type: "urgent", HTML: getLatestNotification() },
+    ];
+
+    const coursesList = [
+      { id: uuidv4(), name: "ES6", credit: 60 },
+      { id: uuidv4(), name: "Webpack", credit: 20 },
+      { id: uuidv4(), name: "React", credit: 40 },
+    ];
+
+    return (
+      <>
+        <div className="root-notifications">
+          <Notifications
+            notifications={notificationsList}
+            displayDrawer={true}
+          />
+        </div>
+        <Header />
+        <div className="mainSection">
+          {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
+        </div>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
