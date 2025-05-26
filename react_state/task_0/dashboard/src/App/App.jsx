ac1b1eef63dd24b3010faa075860a1f95a379687
Khaled-J7 <6711@holbertonstudents.com>
2025-05-26 17:26:24 +0100

    0. Adding a local state for notifications

--- react_state/task_0/dashboard/src/App/App.jsx
+++ react_state/task_0/dashboard/src/App/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import Header from '../Header/Header';
 import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 import { StyleSheet, css } from 'aphrodite';
 
@@ -15,64 +15,49 @@ const listCourses = [
 	{ id: 2, name: 'Webpack', credit: 20 },
 	{ id: 3, name: 'React', credit: 40 },
 ];
+
 const listNotifications = [
 	{ id: 1, type: 'default', value: 'New course available' },
-	{ id: 2, type: 'default', value: 'New resume available' },
+	{ id: 2, type: 'urgent', value: 'New resume available' },
 	{ id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
 ];
 
-const styles = StyleSheet.create({
-	app: {
-		display: 'flex',
-		flexDirection: 'column',
-		textAlign: 'center',
-		backgroundColor: 'rgb(255, 255, 255)',
-		justifyContent: 'space-between',
-		minHeight: '100vh',
-		fontFamily: 'sans-serif',
-	},
-	body: {
-		flexGrow: 1,
-		padding: '20px',
-	},
-	footer: {
-		alignSelf: 'stretch',
-		borderTopStyle: 'solid',
-		borderColor: '#e62d2d',
-		fontStyle: 'italic',
-	},
-});
+document.body.style.margin = 0;
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			displayDrawer: false,
-		};
+		this.handleKeyCombination = this.handleKeyCombination.bind(this);
 		this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
 		this.handleHideDrawer = this.handleHideDrawer.bind(this);
+		this.state = { displayDrawer: false };
 	}
-	componentDidMount() {
-		window.addEventListener('keydown', this.handleKeyDown);
-	}
-	componentWillUnmount() {
-		window.removeEventListener('keydown', this.handleKeyDown);
-	}
-	handleKeyDown = (event) => {
-		if (event.ctrlKey && event.key === 'h') {
+
+	handleKeyCombination(e) {
+		if (e.key === 'h' && e.ctrlKey) {
 			alert('Logging you out');
 			this.props.logOut();
 		}
-	};
-	handleDisplayDrawer = () => {
+	}
+
+	handleDisplayDrawer() {
 		this.setState({ displayDrawer: true });
-	};
-	handleHideDrawer = () => {
+	}
+
+	handleHideDrawer() {
 		this.setState({ displayDrawer: false });
-	};
+	}
+
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleKeyCombination);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleKeyCombination);
+	}
 
 	render() {
-		const { isLoggedIn } = this.props;
+		const { isLoggedIn, logOut } = this.props;
 		const { displayDrawer } = this.state;
 
 		return (
@@ -83,33 +68,36 @@ class App extends Component {
 					handleDisplayDrawer={this.handleDisplayDrawer}
 					handleHideDrawer={this.handleHideDrawer}
 				/>
-				<div className={css(styles.app)}>
-					<Header />
-					<div className={css(styles.body)}>
-						{isLoggedIn ? (
+				<div className={css(styles.container)}>
+					<div className={css(styles.app)}>
+						<Header />
+					</div>
+					<div className={css(styles.appBody)}>
+						{!isLoggedIn ? (
+							<BodySectionWithMarginBottom title='Log in to continue'>
+								<Login />
+							</BodySectionWithMarginBottom>
+						) : (
 							<BodySectionWithMarginBottom title='Course list'>
 								<CourseList listCourses={listCourses} />
 							</BodySectionWithMarginBottom>
-						) : (
-							<BodySectionWithMarginBottom
-								title='
-            Log in to continue'
-							>
-								<Login />
-							</BodySectionWithMarginBottom>
 						)}
-						<BodySection title='News from the School'>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-								enim ad minim veniam, quis nostrud exercitation ullamco laboris
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-								sunt in culpa qui officia deserunt mollit anim id est laborum.
-							</p>
-						</BodySection>
 					</div>
+					<BodySection title='News from the School'>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book. It has
+							survived not only five centuries, but also the leap into
+							electronic typesetting, remaining essentially unchanged. It was
+							popularised in the 1960s with the release of Letraset sheets
+							containing Lorem Ipsum passages, and more recently with desktop
+							publishing software like Aldus PageMaker including versions of
+							Lorem Ipsum.
+						</p>
+					</BodySection>
+
 					<div className={css(styles.footer)}>
 						<Footer />
 					</div>
@@ -118,13 +106,53 @@ class App extends Component {
 		);
 	}
 }
-App.propTypes = {
-	isLoggedIn: PropTypes.bool,
-	logOut: PropTypes.func,
-};
+
 App.defaultProps = {
 	isLoggedIn: false,
 	logOut: () => {},
 };
+
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+	logOut: PropTypes.func,
+};
+
+const cssVars = {
+	mainColor: '#e01d3f',
+};
+
+const screenSize = {
+	small: '@media screen and (max-width: 900px)',
+};
+
+const styles = StyleSheet.create({
+	container: {
+		width: 'calc(100% - 16px)',
+		marginLeft: '8px',
+		marginRight: '8px',
+	},
+
+	app: {
+		borderBottom: `3px solid ${cssVars.mainColor}`,
+	},
+
+	appBody: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
+
+	footer: {
+		borderTop: `3px solid ${cssVars.mainColor}`,
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		position: 'fixed',
+		bottom: 0,
+		fontStyle: 'italic',
+		[screenSize.small]: {
+			position: 'static',
+		},
+	},
+});
 
 export default App;
--- react_state/task_0/dashboard/src/Notifications/Notifications.jsx
+++ react_state/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -2,89 +2,15 @@ import React, { Component } from 'react';
 import NotificationItem from './NotificationItem';
 import PropTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
-import { css, StyleSheet } from 'aphrodite';
-
-const opacityAnimation = {
-	'0%': { opacity: 0.5 },
-	'100%': { opacity: 1 },
-};
-const bounceAnimation = {
-	'0%': { transform: 'translateY(0px)' },
-	'50%': { transform: 'translateY(5px)' },
-	'100%': { transform: 'translateY(-5px)' },
-};
-
-const styles = StyleSheet.create({
-	notification: {
-		padding: '10px',
-		paddingTop: '0px',
-		border: '1.5px dashed #ec1717',
-		position: 'fixed',
-		right: '5px',
-		top: '45px',
-		width: '350px',
-		height: '105px',
-		fontSize: '15px',
-		zIndex: 1000,
-		'@media (max-width: 900px)': {
-			border: 'none',
-			width: '100vw',
-			position: 'relative',
-			left: '-3vw',
-			height: '100vh',
-			fontSize: '20px',
-			padding: '10px',
-			top: 0,
-			backgroundColor: 'white',
-		},
-	},
-	menuItem: {
-		float: 'right',
-		backgroundColor: '#fff8f8',
-		marginRight: '3px',
-		fontWeight: 'bold',
-		position: 'absolute',
-		top: '0px',
-		right: '10px',
-		':hover': {
-			cursor: 'pointer',
-			animationName: [opacityAnimation, bounceAnimation],
-			animationDuration: '1s, 0.5s',
-			animationIterationCount: '3, 3',
-		},
-	},
-	hideItem: {
-		display: 'none',
-	},
-	ul: {
-		listStyleType: 'none',
-		padding: 12,
-		top: 0,
-		margin: '10px',
-	},
-	closeButton: {
-		position: 'absolute',
-		right: '5px',
-		top: '5px',
-		cursor: 'pointer',
-		backgroundColor: 'transparent',
-		border: 'none',
-		'@media (max-width: 900px)': {
-			fontSize: '15px',
-			top: '0px',
-			right: '30px',
-		},
-	},
-});
+import closeIcon from '../assets/close-icon.png';
+import { StyleSheet, css } from 'aphrodite';
 
 class Notifications extends Component {
 	constructor(props) {
 		super(props);
 		this.markAsRead = this.markAsRead.bind(this);
 	}
-	markAsRead(id) {
-		console.log(`Notification ${id} has been marked as read`);
-	}
+
 	shouldComponentUpdate(nextProps) {
 		return (
 			nextProps.listNotifications.length >
@@ -93,51 +19,69 @@ class Notifications extends Component {
 		);
 	}
 
+	markAsRead(id) {
+		console.log(`Notification ${id} has been marked as read`);
+	}
+
 	render() {
 		const {
-			listNotifications,
 			displayDrawer,
+			listNotifications,
 			handleDisplayDrawer,
 			handleHideDrawer,
 		} = this.props;
-		const itemDisplay = displayDrawer ? styles.hideItem : styles.menuItem;
+
+		const menuPStyle = css(
+			displayDrawer ? styles.menuItemPNoShow : styles.menuItemPShow
+		);
 
 		return (
 			<>
 				<div
-					className={css(itemDisplay)}
+					className={css(styles.menuItem)}
 					id='menuItem'
 					onClick={handleDisplayDrawer}
 				>
-					<p>Your notifications</p>
+					<p className={menuPStyle}>Your notifications</p>
 				</div>
 				{displayDrawer && (
-					<div className={css(styles.notification)} id='Notifications'>
-						<p>Here is the list of notifications</p>
-						<ul className={css(styles.ul)}>
-							{listNotifications.length === 0 ? (
-								<NotificationItem value='No new notification for now' />
-							) : (
-								listNotifications.map((notification) => (
-									<NotificationItem
-										key={notification.id}
-										id={notification.id}
-										type={notification.type}
-										value={notification.value}
-										html={notification.html}
-										markAsRead={this.markAsRead}
-									/>
-								))
-							)}
-						</ul>
+					<div className={css(styles.notifications)} id='Notifications'>
 						<button
-							className={css(styles.closeButton)}
+							style={{
+								background: 'transparent',
+								border: 'none',
+								position: 'absolute',
+								right: 20,
+							}}
 							aria-label='close'
-							id='closeButton'
 							onClick={handleHideDrawer}
+							id='closeNotifications'
 						>
-							x
+							<img
+								src={closeIcon}
+								alt='close-icon'
+								className={css(styles.notificationsButtonImage)}
+							/>
 						</button>
+						<p className={css(styles.notificationsP)}>
+							Here is the list of notifications
+						</p>
+						<ul className={css(styles.notificationsUL)}>
+							{listNotifications.length === 0 && (
+								<NotificationItem value='No new notification for now' />
+							)}
+
+							{listNotifications.map((notification) => (
+								<NotificationItem
+									key={notification.id}
+									id={notification.id}
+									type={notification.type}
+									value={notification.value}
+									html={notification.html}
+									markAsRead={this.markAsRead}
+								/>
+							))}
+						</ul>
 					</div>
 				)}
 			</>
@@ -145,17 +89,131 @@ class Notifications extends Component {
 	}
 }
 
-Notifications.propTypes = {
-	displayDrawer: PropTypes.bool,
-	listNotifications: PropTypes.arrayOf(NotificationItemShape),
-	handleDisplayDrawer: PropTypes.func,
-	handleHideDrawer: PropTypes.func,
-};
 Notifications.defaultProps = {
 	displayDrawer: false,
 	listNotifications: [],
 	handleDisplayDrawer: () => {},
 	handleHideDrawer: () => {},
 };
+
+Notifications.propTypes = {
+	displayDrawer: PropTypes.bool,
+	listNotifications: PropTypes.arrayOf(NotificationItemShape),
+	handleDisplayDrawer: PropTypes.func,
+	handleHideDrawer: PropTypes.func,
+};
+
+const cssVars = {
+	mainColor: '#e01d3f',
+};
+
+const screenSize = {
+	small: '@media screen and (max-width: 900px)',
+};
+
+const opacityKeyframes = {
+	from: {
+		opacity: 0.5,
+	},
+
+	to: {
+		opacity: 1,
+	},
+};
+
+const translateYKeyframes = {
+	'0%': {
+		transform: 'translateY(0)',
+	},
+
+	'50%': {
+		transform: 'translateY(-5px)',
+	},
+
+	'75%': {
+		transform: 'translateY(5px)',
+	},
+
+	'100%': {
+		transform: 'translateY(0)',
+	},
+};
+
+const borderKeyframes = {
+	'0%': {
+		border: `3px dashed deepSkyBlue`,
+	},
+
+	'100%': {
+		border: `3px dashed ${cssVars.mainColor}`,
+	},
+};
+
+const styles = StyleSheet.create({
+	menuItem: {
+		float: 'right',
+		backgroundColor: '#fff8f8',
+		':hover': {
+			cursor: 'pointer',
+			animationName: [opacityKeyframes, translateYKeyframes],
+			animationDuration: '1s, 0.5s',
+			animationIterationCount: 3,
+		},
+	},
+
+	menuItemPNoShow: {
+		marginRight: '8px',
+		display: 'none',
+	},
+
+	menuItemPShow: {
+		marginRight: '8px',
+	},
+
+	notifications: {
+		float: 'right',
+		// border: `3px dashed ${cssVars.mainColor}`,
+		padding: '10px',
+		marginBottom: '20px',
+		animationName: [borderKeyframes],
+		animationDuration: '0.8s',
+		animationIterationCount: 1,
+		animationFillMode: 'forwards',
+		':hover': {
+			border: `3px dashed deepSkyBlue`,
+			// animationFillMode: "forwards",
+		},
+		[screenSize.small]: {
+			float: 'none',
+			border: 'none',
+			listStyle: 'none',
+			padding: 0,
+			fontSize: '20px',
+			':hover': {
+				border: 'none',
+				// animationFillMode: "forwards",
+			},
+			position: 'absolute',
+			background: 'white',
+			height: '110vh',
+			width: '100vw',
+		},
+	},
+
+	notificationsButtonImage: {
+		width: '10px',
+	},
+
+	notificationsP: {
+		margin: 0,
+		marginTop: '15px',
+	},
+
+	notificationsUL: {
+		[screenSize.small]: {
+			padding: 0,
+		},
+	},
+});
 
 export default Notifications;
